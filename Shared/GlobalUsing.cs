6a4517f9d626ccbba40db471a5819f3ecef07450
kaeppen <seho@itu.dk>
2021-12-17 15:33:39 +0100

    repo switch

---
+++ Shared/GlobalUsing.cs
@@ -0,0 +1,34 @@
+global using System.ComponentModel.DataAnnotations;
+
+
+global using System;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+/*
+global using System.Linq;
+global using System.Net;
+global using System.Text;
+global using System.ComponentModel.DataAnnotations;
+global using System.Collections.Generic;
+global using System.Reflection.Emit;
+
+global using Moq;
+
+global using Microsoft.Data.Sqlite;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Identity.Web.Resource;
+
+*/
