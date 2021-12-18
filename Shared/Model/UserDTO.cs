3c36da6a7dba1488feb95cb4e0296dcef2b6d448
kaeppen <seho@itu.dk>
2021-12-18 10:46:52 +0100

    progress

--- Shared/Model/UserDTO.cs
+++ Shared/Model/UserDTO.cs
@@ -17,12 +17,12 @@
     public record UserCreateDTO
     {
         [StringLength(50)]
-        public string UserName { get; init; }
-        public string UserType { get; init; }
+        public string UserName { get; set; }
+        public string UserType { get; set; }
         [EmailAddress]
         [DataType(DataType.EmailAddress)]
         [Required]
-        public string Email { get; init; }
+        public string Email { get; set; }
     }
 
 
