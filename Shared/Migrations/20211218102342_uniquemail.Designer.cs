37327b90d8f4c541d2ec075a82c6ce76270699d2
kaeppen <seho@itu.dk>
2021-12-18 11:25:13 +0100

    migration

--- Shared/Migrations/20211218102342_uniquemail.Designer.cs
+++ Shared/Migrations/20211218102342_uniquemail.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace YoghurtBank.Shared.Migrations
 {
     [DbContext(typeof(YoghurtContext))]
-    partial class YoghurtContextModelSnapshot : ModelSnapshot
+    [Migration("20211218102342_uniquemail")]
+    partial class uniquemail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
